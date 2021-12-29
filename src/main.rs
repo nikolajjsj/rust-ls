f08b5c2bf3c263b514b38a3dd85a78768d96c4b5
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-12-29 13:43:16 +0100

    feature: Add more details to output

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
+use chrono::{DateTime, Local};
 use std::error::Error;
 use std::fs;
 use std::fs::DirEntry;
 use std::path::Path;
+use std::path::PathBuf;
 use std::process;
+use structopt::StructOpt;
+
+#[macro_use]
+#[derive(StructOpt, Debug)]
+struct Opt {
+    /// Output file
+    #[structopt(default_value = ".", parse(from_os_str))]
+    path: PathBuf,
+}
 
 fn main() {
-    if let Err(ref e) = run(Path::new(".")) {
+    let opt = Opt::from_args();
+
+    if let Err(ref e) = run(&opt.path) {
         println!("{}", e);
         process::exit(1);
     }
 }
 
-fn run(dir: &Path) -> Result<(), Box<dyn Error>> {
+fn run(dir: &PathBuf) -> Result<(), Box<dyn Error>> {
     if !dir.is_dir() {
         println!("Directory not found");
         process::exit(1);
@@ -25,17 +38,20 @@ fn run(dir: &Path) -> Result<(), Box<dyn Error>> {
 }
 
 fn ls(entry: DirEntry) {
-    let path = entry.path();
     let file_name = entry
         .file_name()
         .into_string()
         .or_else(|f| Err(format!("Invalid entry: {:?}", f)))
         .unwrap();
 
-    let mut output_string = String::from("");
-    if path.is_dir() {
-        output_string.push_str("/");
-    }
-    output_string.push_str(&file_name);
-    println!("{}", output_string);
+    let metadata = entry.metadata().unwrap();
+    let size = metadata.len();
+    let modified: DateTime<Local> = DateTime::from(metadata.modified().unwrap());
+
+    println!(
+        "{:>5} {} {}",
+        size,
+        modified.format("%_d %b %H:%M").to_string(),
+        file_name
+    );
 }
