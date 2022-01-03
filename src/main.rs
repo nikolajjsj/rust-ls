3f71dfba85388eb96b5b1608b569467ca3a59ec2
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2022-01-03 20:29:57 +0100

    feature: Finish up playful ls implementation

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+#[macro_use]
+extern crate chrono;
+extern crate libc;
+extern crate structopt;
+
 use chrono::{DateTime, Local};
+use libc::{S_IRGRP, S_IROTH, S_IRUSR, S_IWGRP, S_IWOTH, S_IWUSR, S_IXGRP, S_IXOTH, S_IXUSR};
 use std::error::Error;
 use std::fs;
 use std::fs::DirEntry;
+use std::os::unix::fs::PermissionsExt;
 use std::path::PathBuf;
 use std::process;
 use structopt::StructOpt;
 
-#[macro_use]
 #[derive(StructOpt, Debug)]
 struct Opt {
     /// Output file
@@ -17,18 +23,19 @@ struct Opt {
 fn main() {
     let opt = Opt::from_args();
 
-    if let Err(ref e) = run(&opt.path) {
+    if let Err(ref e) = run(&opt) {
         println!("{}", e);
         process::exit(1);
     }
 }
 
-fn run(dir: &PathBuf) -> Result<(), Box<dyn Error>> {
-    if !dir.is_dir() {
+fn run(opt: &Opt) -> Result<(), Box<dyn Error>> {
+    let path = &opt.path;
+    if !path.is_dir() {
         println!("Directory not found");
         process::exit(1);
     } else {
-        for entry in fs::read_dir(dir)? {
+        for entry in fs::read_dir(path)? {
             ls(entry?);
         }
     }
@@ -44,13 +51,36 @@ fn ls(entry: DirEntry) {
         .unwrap();
 
     let metadata = entry.metadata().unwrap();
+    let mode = metadata.permissions().mode();
     let size = metadata.len();
     let modified: DateTime<Local> = DateTime::from(metadata.modified().unwrap());
 
     println!(
-        "{:>5} {} {}",
+        "{} {:>5} {} {}",
+        parse_permissions(mode as u16),
         size,
         modified.format("%_d %b %H:%M").to_string(),
         file_name
     );
+}
+
+fn parse_permissions(mode: u16) -> String {
+    let user = triplet(mode, S_IRUSR, S_IWUSR, S_IXUSR);
+    let group = triplet(mode, S_IRGRP, S_IWGRP, S_IXGRP);
+    let other = triplet(mode, S_IROTH, S_IWOTH, S_IXOTH);
+    [user, group, other].join("")
+}
+
+fn triplet(mode: u16, read: u16, write: u16, execute: u16) -> String {
+    match (mode & read, mode & write, mode & execute) {
+        (0, 0, 0) => "---",
+        (_, 0, 0) => "r--",
+        (0, _, 0) => "-w-",
+        (0, 0, _) => "--x",
+        (_, 0, _) => "r-x",
+        (_, _, 0) => "rw-",
+        (0, _, _) => "-wx",
+        (_, _, _) => "rwx",
+    }
+    .to_string()
 }
